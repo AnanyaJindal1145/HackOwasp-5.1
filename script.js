10aae42afe3195ff121a8ec59d859821a2235851
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2023-07-12 14:37:38 +0530

    added js (incomplete yet)

---
+++ script.js
@@ -0,0 +1,15 @@
+$(".red").mouseover(function(){
+    $(".red").addClass("all-hover");
+})
+
+$(".red").mouseout(function(){
+    $(".red").removeClass("all-hover");
+})
+
+$(".blue").mouseover(function(){
+    $(".blue").addClass("all-hover");
+})
+
+$(".blue").mouseout(function(){
+    $(".blue").removeClass("all-hover");
+})
