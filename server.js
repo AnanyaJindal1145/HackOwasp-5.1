6b9b473fd58893373d57b6f5f2f4432d22acdd44
arushi-midha <118929799+arushi-midha@users.noreply.github.com>
2023-07-15 02:19:46 +0530

    final

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
+
+
+app.get("/",(req,res)=>{
+    res.sendFile(login-form-page-final.html);
+
+    });
+
+app.listen(5000,function(){
+    console.log("Server started on port 5000");
+})
